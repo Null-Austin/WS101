c08091a4e02c19189ec6b622f6cc802314a5f9cc
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2025-09-09 15:48:22 -0500

    set up simple site

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
+const port = 3000;
+
 const express = require("express");
 const app = express();
 
 const path = require("node:path")
 
 app.get("/",(req,res,next)=>{
-    res.redirect("/chat")
+    res.redirect("/chat");
 })
 app.get("/chat",(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'chat.html'))
+    res.sendFile(path.join(__dirname,'chat.html'));
+})
+
+app.listen(port,e=>{
+    if (e){
+        return console.error(`Error occured: `,e)
+    }
+    console.log("Hosting on port 3000")
 })
