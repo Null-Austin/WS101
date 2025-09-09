9081e19017ad7aa614ba26b95a5e68df844f105e
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2025-09-09 16:42:09 -0500

    updated front end and backend to include ws

--- src/app.js
+++ src/app.js
@@ -3,8 +3,23 @@ const port = 3000;
 const express = require("express");
 const app = express();
 
+const http = require('http')
+const server = http.createServer(app);
+const socketIO = require('socket.io');
+const io = socketIO(server)
+
 const path = require("node:path")
 
+let id = 0
+io.on("connection",(socket)=>{
+    id+=1
+    let _id = id
+    console.log("new connection! ",_id)
+
+    socket.on("disconnect",()=>{
+        console.log("connection left :( ",_id)
+    })
+})
 app.get("/",(req,res,next)=>{
     res.redirect("/chat");
 })
@@ -12,7 +27,7 @@ app.get("/chat",(req,res,next)=>{
     res.sendFile(path.join(__dirname,'chat.html'));
 })
 
-app.listen(port,e=>{
+server.listen(port,e=>{
     if (e){
         return console.error(`Error occured: `,e)
     }
